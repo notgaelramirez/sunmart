23870a716c4345cb02f7bd4ad86a6d37592c6b17
notgaelramirez <gael1423@gmail.com>
2022-07-11 15:26:50 -0500

    cart mqueta

--- src/components/cart/CartScreen.jsx
+++ src/components/cart/CartScreen.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import ItemScreen from './ItemScreen'
 
 const CartScreen = () => {
   return (
-    <div>CartScreen</div>
+    <article className='cart'>
+      <header className='cart-header'>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <p>Shopping Cart</p>
+      </header>
+
+      <div className="items">
+        <ItemScreen />
+        <ItemScreen />
+        <ItemScreen />
+        <ItemScreen />
+        <ItemScreen />
+        <ItemScreen />
+      </div>
+    </article>
   )
 }
 
---
+++ src/components/cart/ItemScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemScreen = () => {
+  return (
+    <div className='item'>ItemScreen</div>
+  )
+}
+
+export default ItemScreen
